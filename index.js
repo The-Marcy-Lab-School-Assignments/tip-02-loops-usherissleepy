0423bd766da92524ecb5ffe6c78cd925d5d9c642
[usherissleepy] <[ashar.zarrar218@gmail.com]>
2025-10-01 14:19:33 -0400

    tip challenge number two (three)

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 // Write a function that prints the first n square numbers and also calculates the sum of these numbers.
+let square
 const printSquareNumbers = (n) => {
-  //write code here
+  for (let i = 1; i <= n; i++) {
+    let square = i * i
+    console.log(square)
+  }
 
 }
-printSquareNumbers(5);
+
+printSquareNumbers(5)
 //will print: 1, 4, 9, 16, 25
 // Final output is: 55
